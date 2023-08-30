89897be91265d5796ef20b5af85acd8a3d8f12a9
kaviyarasan.k <143425090+KaviyarasanFSD12@users.noreply.github.com>
2023-08-30 23:30:31 +0530

    Add files via upload

---
+++ Json.js
@@ -0,0 +1,15 @@
+const jsonData = {
+    string: "kavin",
+    number: 02,
+    array:["kavin",02,{nestedObj : "nestedValue"}],
+    Obj:{
+        name: "hari",
+        Friend: "kavi",
+        Office:["thara","swathi","pavi"],
+    },
+    isStudent: true,
+};
+console.log(jsonData);
+console.log(jsonData.array[1],nestedObj);
+console.log(jsonData.Obj.Office[0]);
+console.log(jsonData.);
